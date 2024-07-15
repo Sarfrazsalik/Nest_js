78593c934c334bd7cd0ad4a253c3ccee3f7d0a68
Md Salik Sarfraz <136110826+Sarfrazsalik@users.noreply.github.com>
2024-07-15 18:07:05 +0530

    update

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
 import { DatabaseService } from 'src/database/database.service';
-import { Prisma } from '@prisma/client/edge';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class TodoService {
